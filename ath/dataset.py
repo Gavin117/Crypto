ef541155e2a6635e66105d851a0bb3fd29aecf60
Gavin117 <100494117+Gavin117@users.noreply.github.com>
2022-08-04 21:53:50 +0100

    Update dataset.py

--- ath/dataset.py
+++ ath/dataset.py
@@ -4,14 +4,10 @@ dataset = [
     {'name':'XRP','api':'ripple','ath':3.40},
     {'name':'XMR','api':'monero','ath':542.33},
     {'name':'XLM','api':'stellar','ath':0.875},
-    
-    #exchanges
     {'name':'BNB','api':'binancecoin','ath':686.31},
     {'name':'FTX','api':'ftx-token','ath':84.18},
     {'name':'KCS','api':'kucoin-shares','ath':28.83},
     {'name':'LEO','api':'leo-token','ath':8.14},
-    
-    #layer1
     {'name':'ADA','api':'cardano','ath':3.09},
     {'name':'SOL','api':'solana','ath':260.06},
     {'name':'AVAX','api':'avalanche-2','ath':144.96},
@@ -19,16 +15,10 @@ dataset = [
     {'name':'NEAR','api':'near','ath':20.44},
     {'name':'FLOW','api':'flow','ath':42.40},
     {'name':'KLAY','api':'klay-token','ath':4.34},
-    
-    #layer2
     {'name':'LINK','api':'chainlink','ath':52.70},
     {'name':'GRT','api':'the-graph','ath':2.84},
-    
-    #meme
     {'name':'DOGE','api':'dogecoin','ath':0.73},
     {'name':'SHIB','api':'shiba-inu','ath':0.00008616},
-    
-    #defi
     {'name':'UNI','api':'uniswap','ath':44.92},
     {'name':'CAKE','api':'pancakeswap-token','ath':43.96},
     {'name':'1INCH','api':'1inch','ath':8.65},
@@ -36,30 +26,16 @@ dataset = [
     {'name':'OSMO','api':'osmosis','ath':11.25},
     {'name':'AAVE','api':'aave','ath':661.69},
     {'name':'RUNE','api':'thorchain','ath':20.87},
-    
-    #nfts
     {'name':'ENJ','api':'enjincoin','ath':4.82},
     {'name':'SAND','api':'the-sandbox','ath':8.40},
     {'name':'MANA','api':'decentraland','ath':5.85},
     {'name':'APE','api':'apecoin','ath':26.47},
-    
-    #media
     {'name':'THETA','api':'theta-token','ath':15.72},
-    
-    #gaming
     {'name':'GALA','api':'gala','ath':0.825},
-    
-    #storage
     {'name':'AR','api':'arweave','ath':89.24},
     {'name':'FIL','api':'filecoin','ath':236.84},
-    
-    #loans
     {'name':'NEXO','api':'nexo','ath':4.07},
-
-    #supply chain
     {'name':'VET','api':'vechain','ath':0.281},
-    
-    #other
     {'name':'RVN','api':'ravencoin','ath':0.285218},
     {'name':'ELGD','api':'elrond-erd-2','ath':545.64},
     {'name':'ATOM','api':'cosmos','ath':44.45},
